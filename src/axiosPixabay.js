323fd8c40659b5e21f7df7e1f2b7b4c84134f3ed
Anastasiia Tyshkevych <anastasia.tyshkevych@gmail.com>
2022-01-24 10:27:27 +0200

    upd-gallery

--- src/axiosPixabay.js
+++ src/axiosPixabay.js
@@ -1,37 +1,46 @@
-const axios = require('axios');
+import axios from 'axios';
+
+
+// const axios = require('axios');
 const API_KEY = '25313098-4fe256b98efcc708221cd816f';
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/'
 
 export default class ImageApiService {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-    this.per_page = 40;
-  }
-  async fetchFotos() {
-    const parameters = new URLSearchParameters({
-      q: this.searchQuery,
-      key: API_KEY,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: this.per_page,
-      page: this.page,
-    });
-    const url = `${BASE_URL}/?${parameters}`;
-    this.icrementPage();
-    return await axios.get(url);
-  }
-  icrementPage() {
+    // constructor() {
+    //     this.searchQuery = '';
+    //     this.page = 1;
+    //     this.per_page = 40;
+    // }
+    // async fetchPhotos() {
+    // const PARAMETRES = `key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.per_page}&page=${this.page}`
+    // return await axios.get(`${BASE_URL}, ${PARAMETRES}`)
+    // .then(res => res.data)
+    // }
+    async fetchPhotos() {
+        const params = new URLSearchParams({
+          q: this.searchQuery,
+          key: '25154920-bc2b97b916e9c15e1ff6fb5dd',
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: this.per_page,
+          page: this.page,
+        });
+        const url = `${BASE_URL}/?${params}`;
+        this.icrementPage();
+        return await axios.get(url);
+      }
+    icrementPage() {
     this.page += 1;
-  }
-  resetPage() {
+    }
+    resetPage() {
     this.page = 1;
-  }
-  get query() {
+    }
+    get query() {
     return this.searchQuery;
-  }
-  set query(newQuery) {
+    }
+    set query(newQuery) {
     this.query - newQuery;
-  }
-}
+    }
+}  
